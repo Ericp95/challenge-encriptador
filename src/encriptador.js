68699a8e55b9c501cb1cf6f33b6e468a01ad986d
Ericp95 <ericp6960@gmail.com>
2024-03-15 10:59:54 -0300

    funcion para desencritar funcionando

--- src/encriptador.js
+++ src/encriptador.js
@@ -47,12 +47,45 @@ function encriptar(traduccion){
 
 }
 
+function desencriptar(traduccion){
+    document.querySelector("#warning").removeAttribute("style");
+    var textarea = document.querySelector("#texto");
+    var texto = textarea.value;
+    var area_default = document.querySelector("#default");
+    var area_result = document.querySelector("#result");
+    var texto_out = document.querySelector("#texto_out");
+    if (texto != ""){
+        for(var i=0; i < texto.length; i++){
+            if(((texto[i] < 'a') || (texto[i] > 'z')) && (texto[i] != ' ')){
+                document.querySelector("#warning").style.color = "red";
+                document.querySelector("#warning").style.fontSize = "16px";
+                return;
+            }
+            else if((texto.length == 1 && texto == " ") || texto.replace(/ /g, "") == ""){
+                area_default.classList.remove("invisible");
+                area_result.classList.add("invisible");
+                return;
+            }
+        }
+        area_default.classList.add("invisible");
+        area_result.classList.remove("invisible");
+        texto = texto.replace(new RegExp(traduccion["a"], "g"), "a");
+        texto = texto.replace(new RegExp(traduccion["e"], "g"), "e");
+        texto = texto.replace(new RegExp(traduccion["i"], "g"), "i");
+        texto = texto.replace(new RegExp(traduccion["o"], "g"), "o");
+        texto = texto.replace(new RegExp(traduccion["u"], "g"), "u");
+        texto_out.innerHTML = texto;
+    }
+    return;
+}
+
 
 
 
 const enc = document.querySelector('#enc');
-
+const des = document.querySelector('#des');
 
 var traduccion = {"a": "ai", "e": "enter", "i": "imes", "o": "ober", "u": "ufat"};
 
 enc.addEventListener( 'click', function() {encriptar(traduccion);} );
+des.addEventListener( 'click', function() {desencriptar(traduccion);} );
